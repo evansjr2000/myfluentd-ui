3421b688967331815f197c7af213c57dfad5e742
John R Evans <John.Evans97@gmail.com>
2023-03-10 14:38:13 -0600

    Standard update in fluentd-ui---Fri Mar 10 14:38:13 CST 2023.

--- fluentd-ui/docker-build.sh
+++ fluentd-ui/docker-build.sh
@@ -3,4 +3,4 @@ export VERS="v0.3-test"
 docker build -t fluentd-ui .
 docker tag fluentd-ui evansjr/myfluentd-ui:${VERS}
 
-# docker push evansjr/myfluentd-ui:${VERS}
+docker push evansjr/myfluentd-ui:${VERS}
