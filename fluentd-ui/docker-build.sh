63af95f420b4a35c4023f2159a4f1459b9e594ae
John R Evans <John.Evans97@gmail.com>
2023-03-13 09:33:40 -0500

    Standard update in fluentd---Mon Mar 13 09:33:39 CDT 2023.

--- fluentd-ui/docker-build.sh
+++ fluentd-ui/docker-build.sh
@@ -1,5 +1,5 @@
 #/bin/bash -x
-export VERS="v0.3-test"
+export VERS="v0.4"
 docker build -t fluentd-ui .
 docker tag fluentd-ui evansjr/myfluentd-ui:${VERS}
 
--- fluentd-ui/docker-run.sh
+++ fluentd-ui/docker-run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -x
 
-export VERS="v0.3-test"
+export VERS="v0.4"
 
 
 # docker build -t fluent/fluentd-ui:1.0.0 .
 
 # docker run --net=host evansjr/myfluentd-ui:${VERS}
-docker run -it --name myfluentd-ui  -p 9292:9292 -v /var/log:/var/log2 -v `pwd`/tmp/fluentd:/etc/fluentd evansjr/myfluentd-ui:${VERS} /bin/bash
+docker run -it --name myfluentd-ui  \
+       -p 9292:9292 \
+       -v /var/log:/var/log2 \
+       -v `pwd`/tmp/fluentd:/etc/fluentd \
+       -e THOR_SILENCE_DEPRECATION=True \
+       evansjr/myfluentd-ui:${VERS}
 
 
 
